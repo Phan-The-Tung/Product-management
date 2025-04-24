06450e28c57ef12ff805c3ff8ef9900ec62657f8
Phan-The-Tung <phantung17072004@gmail.com>
2025-04-24 23:30:32 +0700

    ket noi mongodb

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+require("dotenv").config();
 
+const database = require("./config/database");
 const route = require("./routes/client/index.route");
+ 
+
+ 
+
+database.connect();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 
 
