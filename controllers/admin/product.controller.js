90f3b57862f8685ddc874d75284f2bb71546c2c9
Phan-The-Tung <phantung17072004@gmail.com>
2025-05-06 13:53:39 +0700

    xóa mềm 2

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -94,7 +94,7 @@ module.exports.deleteItem = async(req, res) => {
    const id = req.params.id;
     
   //  await Product.deleteOne({_id: id});
-  await Product.updateOne({_id: id}, {deleted: true});
+  await Product.updateOne({_id: id}, {deleted: true},{deletedAt: new Date()});
 
    res.redirect(req.get("referer"));
 }
