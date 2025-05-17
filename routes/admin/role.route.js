aa7b8279bad56ba123addc11e63f681fb098c6e0
Phan-The-Tung <phantung17072004@gmail.com>
2025-05-17 23:57:56 +0700

    Xây dựng danh sách nhóm quyền

---
+++ routes/admin/role.route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const controllers = require("../../controllers/admin/role.controller") ;
+
+
+router.get("/", controllers.index);
+
+router.get("/create", controllers.create);
+
+router.post("/create", controllers.createPost);
+
+module.exports = router;
