d12a5f622e92ec83d826ba68ab6f6e2a560d2aea
Phan-The-Tung <phantung17072004@gmail.com>
2025-05-19 16:52:53 +0700

    Tính năng đăng nhập admin

---
+++ routes/admin/auth.route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const controllers = require("../../controllers/admin/auth.controller") ;
+const validates = require("../../validates/admin/auth.validate");
+
+router.get("/login", controllers.login);
+
+router.post("/login",
+    validates.loginPost,
+    controllers.loginPost);
+
+module.exports = router;
