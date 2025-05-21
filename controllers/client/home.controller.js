3e385a35ae8363188204c95dca3cfc237b18c0bc
Phan-The-Tung <phantung17072004@gmail.com>
2025-05-21 23:55:35 +0700

    Hiển thị menu (Client)

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,16 @@
-module.exports.index = (req, res) => {
+const ProductCategory = require("../../models/product-category.model");
+const createTreeHelper = require("../../helpers/createTree");
+
+
+module.exports.index = async (req, res) => {
+
+    const productsCategory = await ProductCategory.find({
+        deleted: false,
+    })
+
+    const newProductsCategory = createTreeHelper.tree(productsCategory);
+
     res.render("client/pages/home/index", {
-        pageTitle: "Trang chu"
+        pageTitle: "Trang chủ"
     });
 };
