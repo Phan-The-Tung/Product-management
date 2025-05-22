7624144ed042c76cf322c348e565adb46021a031
Phan-The-Tung <phantung17072004@gmail.com>
2025-05-22 22:39:45 +0700

    Hiển thị danh sách sp mới

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -11,8 +11,18 @@ module.exports.index = async (req, res) => {
 
       const newProducts = productsHelper.priceNewProducts(productsFeatured);
 
+      const productsNew = await Product.find({
+        deleted: false,
+        status: "active"
+     }).sort({ position: "desc"}).limit(6);
+
+      const newProductsNew = productsHelper.priceNewProducts(productsNew);
+      
+
+
     res.render("client/pages/home/index", {
         pageTitle: "Trang chủ",
-        productsFeatured: newProducts
+        productsFeatured: newProducts,
+        productsNew: newProductsNew
     });
 };
