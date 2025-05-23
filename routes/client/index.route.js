b3add8790d288fb26416345dcf639657567bac08
Phan-The-Tung <phantung17072004@gmail.com>
2025-05-23 10:02:51 +0700

    Trang kết quả tìm kiếm client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,11 +1,15 @@
 const productRoutes = require("./product.route");
-const homeRouters = require("./home.route");
+const homeRoutes = require("./home.route");
+const searchRoutes = require("./search.route"); 
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
+
 
 module.exports = (app) => {
     app.use(categoryMiddleware.category);
 
-    app.use("/",homeRouters );
+    app.use("/",homeRoutes );
     
     app.use("/product", productRoutes );
+
+    app.use("/search", searchRoutes );
 }
