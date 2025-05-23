8cd505b233facd72458eca3d5cd8b5d2b6f268c4
Phan-The-Tung <phantung17072004@gmail.com>
2025-05-23 21:38:15 +0700

    Cập nhật số lượng sản phẩm trong giỏ hàng

---
+++ public/js/cart.js
@@ -0,0 +1,16 @@
+// Cập nhật số lượng sản phẩm trong giỏ hàng
+const inputsQuantity = document.querySelectorAll("input[name='quantity']");
+if (inputsQuantity.length > 0) {
+  inputsQuantity.forEach(input => {
+    input.addEventListener("change", () => {
+      const productId = input.getAttribute("product-id");
+      const quantity = input.value;
+
+      console.log(productId);
+      console.log(quantity);
+
+      window.location.href = `/cart/update/${productId}/${quantity}`;
+    });
+  });
+}
+// Hết Cập nhật số lượng sản phẩm trong giỏ hàng
