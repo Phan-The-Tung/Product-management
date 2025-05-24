b776004dec373469ab485fa278a0b857d62e28d4
Phan-The-Tung <phantung17072004@gmail.com>
2025-05-24 16:45:20 +0700

    Đăng kí Đăng nhập

---
+++ controllers/client/user.controller.js
@@ -0,0 +1,76 @@
+const User = require("../../models/user.model");
+const md5 = require("md5");
+
+// [GET] /user/register
+module.exports.register = async (req, res) => {
+    res.render("client/pages/user/register", {
+        pageTitle: "Đăng ký"
+    })
+
+}
+
+// [POST] /user/register
+module.exports.registerPost = async (req, res) => {
+    // console.log(req.body);
+    
+    const existEmail = await User.findOne({
+      email: req.body.email
+    });
+  
+    if (existEmail) {
+      req.flash("error", "Email đã tồn tại!");
+      res.redirect(req.get("referer"));
+      return;
+    }
+  
+    req.body.password = md5(req.body.password);
+  
+    const user = new User(req.body);
+    await user.save();
+  
+     res.cookie("tokenUser", user.tokenUser);
+
+     res.redirect("/");
+  };
+
+// [GET] /user/login
+module.exports.login = async (req, res) => {
+    res.render("client/pages/user/login", {
+        pageTitle: "Đăng nhập"
+    })
+
+}
+
+// [POST] /user/login
+module.exports.loginPost = async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    const user = await User.findOne({
+      email: email,
+      deleted: false
+    });
+    
+    if (!user) {
+      req.flash("error", "Email không tồn tại!");
+      res.redirect(req.get("referer"));
+      return;
+    }
+
+    if (md5(password) != user.password) {
+        req.flash("error", "Sai mật khẩu!");
+        res.redirect(req.get("referer"));
+        return;
+    }
+
+    if (user.status === "inactive") {
+        req.flash("error", "Tài khoản đang bị khóa!");
+        res.redirect(req.get("referer"));
+        return;
+    }
+
+    res.cookie("tokenUser", user.tokenUser);
+
+    res.redirect("/");
+}
+  
---
+++ routes/client/user.route.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const controllers = require("../../controllers/client/user.controller")
+const validate = require("../../validates/client/user.validate");
+
+router.get("/register",controllers.register );
+
+router.get("/login",controllers.login );
+
+router.post("/register", validate.registerPost, controllers.registerPost );
+
+router.post("/login", validate.loginPost, controllers.loginPost );
+
+module.exports = router;
