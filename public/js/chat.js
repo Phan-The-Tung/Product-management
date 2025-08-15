eea70745261d5f7a1f0470a0acf77b6b2925f3ff
Phan-The-Tung <phantung17072004@gmail.com>
2025-08-15 08:21:57 +0700

    Typing(P2)

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,7 @@ if (formSendData) {
     if (content) {
       socket.emit("CLIENT_SEND_MESSAGE", content);
       e.target.elements.content.value = "";
+      socket.emit("CLIENT_SEND_TYPING", "hidden");
     }
   });
 }
@@ -20,6 +21,7 @@ if (formSendData) {
 socket.on("SERVER_RETURN_MESSAGE", (data) => {
     const myId = document.querySelector("[my-id]").getAttribute("my-id");
     const body = document.querySelector(".chat .inner-body");
+    const boxTyping = document.querySelector(".chat .inner-list-typing");
   
     const div = document.createElement("div");
     let htmlFullName = "";
@@ -36,11 +38,11 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
       <div class="inner-content">${data.content}</div>
     `;
   
-    body.appendChild(div);
+    body.insertBefore(div, boxTyping);
 
     body.scrollTop = body.scrollHeight;
   });
-  // End SERVER_RETURN_MESSAGE
+// End SERVER_RETURN_MESSAGE
 
   
 
@@ -65,6 +67,19 @@ if (buttonIcon) {
 }
 // End Show Popup
 
+// Show Typing
+var timeOut;
+const showTyping = () => {
+    socket.emit("CLIENT_SEND_TYPING", "show");
+
+    clearTimeout(timeOut);
+
+    timeOut = setTimeout(() => {
+        socket.emit("CLIENT_SEND_TYPING", "hidden");
+    }, 3000);
+};
+// End Show Typing
+
 // Insert Icon To Input
 const emojiPicker = document.querySelector("emoji-picker");
 if (emojiPicker) {
@@ -72,15 +87,60 @@ if (emojiPicker) {
   emojiPicker.addEventListener("emoji-click", (event) => {
     const icon = event.detail.unicode;
     inputChat.value = inputChat.value + icon;
+
+    const end = inputChat.value.length;
+    inputChat.setSelectionRange(end, end);
+    inputChat.focus();
+    showTyping();
   });
+
+   
   
   // Input Keyup
   inputChat.addEventListener("keyup", () => {
-    socket.emit("CLIENT_SEND_TYPING", "show");
+     showTyping();
   });
   // End Input Keyup
   
 }
 // End Insert Icon To Input
 // End Show Icon Chat
+
+
+// SERVER_RETURN_TYPING
+const elementListTyping = document.querySelector(".chat .inner-list-typing");
+
+if(elementListTyping) {
+    socket.on("SERVER_RETURN_TYPING", (data) => {
+        console.log(data);
+        if(data.type == "show") {
+            const bodyChat = document.querySelector(".chat .inner-body");
+            const existTyping = elementListTyping.querySelector(`[user-id="${data.userId}"]`);
+
+            if(!existTyping) {
+                const boxTyping = document.createElement("div");
+                boxTyping.classList.add("box-typing");
+                boxTyping.setAttribute("user-id", data.userId);
+
+                boxTyping.innerHTML = `
+                    <div class="inner-name">${data.fullName}</div>
+                    <div class="inner-dots">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                `;
+                elementListTyping.appendChild(boxTyping);
+                bodyChat.scrollTop = bodyChat.scrollHeight;
+            } else {
+              const boxTypingRemove = elementListTyping.querySelector(`[user-id="${data.userId}"]`);
+              if(boxTypingRemove) {
+                  elementListTyping.removeChild(boxTypingRemove);
+              }
+            }
+        }
+    });
+}
+
+// End SERVER_RETURN_TYPING
   
